337e55c6da82faab88ed0373f9220d7676df60b3
favv <nnwabuwe@gmail.com>
2025-03-18 17:30:47 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'T∆RIS KING', // Your name
-  ownerNumber: '8139539470', //Your number
+  ownerNumber: '2348139539470', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
